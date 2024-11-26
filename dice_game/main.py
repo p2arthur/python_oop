5a078dda3270e57356867699fe7109f6c2d71713
p2arthur <arthurrabelopro@gmail.com>
2024-11-25 20:54:56 -0800

    [refactor:dice-game] refactor dice game

--- dice_game/main.py
+++ dice_game/main.py
@@ -36,7 +36,6 @@ class Player:
 
     def roll_die(self):
        die_value =  self._die.roll_die()
-       return die_value
 
     def increment_counter(self):
         self._counter+=1
@@ -54,67 +53,64 @@ class DiceGame:
         print('=====================================')
         print('Welcome to Roll the Dice game!')
         print('=====================================')
-        while self._player.counter > 0 < self._computer.counter:
+        while True:
             self._start_round_()
-            game_over = self.check_game_over()
+            game_over = self._check_game_over()
             if game_over:
                 break
 
     def _start_round_(self):
-        self.print_round_welcome()
-        player_value = self._player.roll_die()
-        computer_value = self._computer.roll_die()
-        self.print_dice()
-        self.check_winner(player_value, computer_value)
-        self.print_game_score()
+        self._print_round_welcome()
+        self._roll_dice()
+        self._print_dice()
+        self._check_winner()
+        self._print_game_score()
 
     @staticmethod
-    def print_round_welcome():
-        print('--------- NEW ROUND ----------')
+    def _print_round_welcome():
+        print('\n--------- NEW ROUND ----------')
         input('Press any key to roll the dice')
 
-    def print_game_score(self):
-        print(f'Player counter:{self._player.counter} | Computer counter: {self._computer.counter}')
+    def _roll_dice(self):
+        self._player.roll_die()
+        self._computer.roll_die()
 
-    def print_dice(self):
+    def _print_game_score(self):
+        print(f'\nPlayer counter:{self._player.counter} | Computer counter: {self._computer.counter}')
+
+    def _print_dice(self):
         print(f'Player die:{self._player.die.value} | Computer die: {self._computer.die.value}')
 
-    def check_winner(self, player_value, computer_value):
-        if player_value > computer_value:
-            self.update_counter(self._player, self._computer)
-        elif computer_value > player_value:
-            self.update_counter(self._computer, self._player)
+    def _check_winner(self):
+        if self._player.die.value > self._computer.die.value:
+            self._update_players_counter(self._player, self._computer)
+        elif self._computer.die.value > self._player.die.value:
+            self._update_players_counter(self._computer, self._player)
         else:
             print('Its a tie')
 
     @staticmethod
-    def update_counter( winner:Player, loser:Player):
+    def _update_players_counter(winner:Player, loser:Player):
         winner.decrement_counter()
         loser.increment_counter()
 
-    def check_game_over(self)->bool:
+    def _check_game_over(self)->bool:
 
         if self._computer.counter == 0:
             print('---------------------Computer wins-------------------')
-            self.show_game_over(self._computer)
+            self._show_game_over(self._computer)
             return True
         if self._player.counter == 0:
-            self.show_game_over(self._player)
+            self._show_game_over(self._player)
             print('---------------------Player wins---------------------')
             return True
         else:
             return False
 
     @staticmethod
-    def show_game_over(winner:Player):
-        if winner.is_computer:
-            print('-----------------------------------------------')
-            print('Computer wins')
-            print('-----------------------------------------------')
-        else:
-            print('-----------------------------------------------')
-            print('Player wins')
-            print('-----------------------------------------------')
+    def _show_game_over(winner:Player):
+       print(f'{"Computer" if winner.is_computer else "Player"}')
+
 
 
 
